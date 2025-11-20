32e5917c7ce06f77e4ba7a9ca7826ce271f7b1da
ameena954 <ameenafirdous92@gmail.com>
2025-11-20 21:10:20 +0530

    done

---
+++ First_Program.java
@@ -0,0 +1,9 @@
+public class First_Program{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("My name is" +args[0]);
+        System.out.println("I am " +args[1] + "years old");
+
+    }
+}
+
